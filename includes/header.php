f2e3d589ad9ae632292ed6f6184cb135c82426c0
Christian Gianfranco <xepher95@gmail.com>
2015-11-05 10:37:54 -0500

    Header
    De localhost a online

--- includes/header.php
+++ includes/header.php
@@ -10,13 +10,13 @@
     <title>Directorio Barranca</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="/proyecto_barranca/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
 
     <!-- Fonts -->
-    <link href="/proyecto_barranca/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href="/proyecto_barranca/css/animate.css" rel="stylesheet" />
+    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="css/animate.css" rel="stylesheet" />
     <!-- Squad theme CSS -->
-    <link href="/proyecto_barranca/css/style.css" rel="stylesheet">
-	<link href="/proyecto_barranca/color/default.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+	<link href="color/default.css" rel="stylesheet">
 
 </head>
