126cc2fa52149620cc3374ff96d1ab2b860e5d37
Christian Gianfranco <xepher95@gmail.com>
2016-03-23 17:37:11 -0500

    Diseño e imagen

--- templates/contacto.php
+++ templates/contacto.php
@@ -11,91 +11,6 @@
 	?>
 
 	
-
-	<!-- Section: services -->
-    <section id="directorio" class="home-section text-center bg-gray">
-		
-		<div class="heading-about">
-			<div class="container">
-			<div class="row">
-				<div class="col-lg-8 col-lg-offset-2">
-					<div class="wow bounceInDown" data-wow-delay="0.4s">
-					<div class="section-heading">
-					<h2>A BUSCAR!</h2>
-					<h5>Tratamos de que encuentres todo en la comodidad de tu hogar</h5>
-					<i class="fa fa-2x fa-angle-down"></i>
-
-					</div>
-					</div>
-				</div>
-			</div>
-			</div>
-		</div>
-		<div class="container">
-		<div class="row">
-			<div class="col-lg-2 col-lg-offset-5">
-				<hr class="marginbot-50">
-			</div>
-		</div>
-        <div class="row">
-			<div class="col-sm-3 col-md-3">
-				<div class="wow fadeInUp" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-						<!--a href="empleo"--><img class="desaturada" src="img/icons/ser_tra.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Empleo</h5><!--/a-->
-						<p>Encuentra lista de empleos rapidamente en el distrito que te encuentres.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-			<div class="col-sm-3 col-md-3">
-				<div class="wow fadeInUp" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-						<a href="inmueble"><img src="img/icons/ser_ter.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Inmuebles</h5></a>
-						<p>Alquiler de departamentos, cuartos, locales comerciales, etc.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-			<div class="col-sm-3 col-md-3">
-				<div class="wow fadeInRight" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-					<!--a href="terrenos"--><img class="desaturada" src="img/icons/ser_prod.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Terrenos</h5><!--/a-->
-						<p>Compra de terrenos, casas, departamentos.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-            <div class="col-sm-3 col-md-3">
-				<div class="wow fadeInLeft" data-wow-delay="0.2s">
-                <div class="service-box">
-					<div class="service-icon">
-						<!--a href="prod"--><img class="desaturada" src="img/icons/ser_pro.png" alt="" />
-					</div>
-					<div class="service-desc">
-						<h5>Productos y Servicios</h5><!--/a-->
-						<p>Venda sus productos nuevos, ofrezca sus servicios, precios y ubicacion.</p>
-					</div>
-                </div>
-				</div>
-            </div>
-        </div>		
-		</div>
-	</section>
-	<!-- /Section: services -->
-	
-
 	
 
 	<!-- Section: contact -->
@@ -202,7 +117,7 @@
 	</section>
 	<!-- /Section: contact -->
 
-	
+	<section id="contactoIntro" class="contactoIntro"></section>
 
 </body>
 <?php
