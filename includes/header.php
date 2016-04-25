b12e5e1512a356901b627f92fa08225ac3d719b2
Christian Avalos <jl.cnblue@gmail.com>
2016-04-25 11:35:51 -0500

    css

--- includes/header.php
+++ includes/header.php
@@ -9,14 +9,14 @@
     <title>Directorio Norte Chico</title>
 
     <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="/css/bootstrap.css" rel="stylesheet" type="text/css">
 
     <!-- Fonts -->
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
-	<link href="css/animate.css" rel="stylesheet" />
+    <link href="/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+	<link href="/css/animate.css" rel="stylesheet" />
     <!-- Squad theme CSS -->
-    <link href="css/style.css" rel="stylesheet">
-    <link href="js/jquery.fancybox.css" rel="stylesheet">
-	<link href="color/default.css" rel="stylesheet">
+    <link href="/css/style.css" rel="stylesheet">
+    <link href="/js/jquery.fancybox.css" rel="stylesheet">
+	<link href="/color/default.css" rel="stylesheet">
 
 </head>
