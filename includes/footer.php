9c237902ab2b6bcbab350ab3f35f22f15c00a80a
Christian Avalos <jl.cnblue@gmail.com>
2016-04-25 11:36:18 -0500

    js

--- includes/footer.php
+++ includes/footer.php
@@ -16,11 +16,11 @@
 	</footer>
 
     <!-- Core JavaScript Files -->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery.fancybox.pack.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.easing.min.js"></script>	
-	<script src="js/jquery.scrollTo.js"></script>
-	<script src="js/wow.min.js"></script>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/jquery.fancybox.pack.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/jquery.easing.min.js"></script>	
+	<script src="/js/jquery.scrollTo.js"></script>
+	<script src="/js/wow.min.js"></script>
     <!-- Custom Theme JavaScript -->
-    <script src="js/custom.js"></script>
+    <script src="/js/custom.js"></script>
